4fdba440903c96d1608180719d7b22ac324e8646
Bhanu <bhanu@krenovate.com>
2022-11-08 22:44:22 +0530

    fix: Used env files instead of hardcoded API key

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import { createClient } from "contentful"
 
 // This is a test integration and these tokens will be moved to ENV file.
 const client = createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
-  space: "95q77usk8v4x",
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: "VjwnbCFa9o56oT8qL8O53hueE_5Vig35E_fhnP2EI6E"
+  space: import.meta.env.VITE_CONTENTFUL_SPACE || '',
+  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESSTOKEN || ''
 });
 
 client.getEntry('5KBaKfSYX4wm0OqOFmyRS1')
